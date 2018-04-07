170d37d5bdfedbc09b1f2b18dfb9666906b0d3b6
leslie-huang <lesliehuang@gmail.com>
2018-04-07 19:34:45 -0400

    auto tag some training data

--- generate-corpus.py
+++ generate-corpus.py
@@ -5,6 +5,7 @@
 import argparse
 import collections
 import itertools
+import numpy as np
 import os
 import re
 import sys
@@ -13,7 +14,23 @@ import pandas as pd
 
 un_corpus = pd.read_csv("un_corpus.csv")
 un_corpus.drop(["filename", "record_name", "speaker_notes"], axis = 1)
-un_corpus_2000 = un_corpus[un_corpus["year"] >= 2000]
+
+# So that training/test data cover some overlapping countries/years, use a subset of the original data
+# countries_list = np.random.choice(un_corpus["country"].unique(), 50, False)
+countries_list = ["Afghanistan", "Algeria", "Argentina", "Austalia",
+    "Bahrain", "Bhutan", "Brazil", "Bulgaria",
+    "Canada", "Chile", "Chad", "China", "Colombia",
+    "Denmark", "Egypt", "El Salvador", "Ethiopia", "France",
+    "Germany", "Ghana", "Iceland", "India", "Iraq", "Japan",
+    "Kuwait", "Libya", "Mali", "Mexico", "Myanmar", "Morocco", "Mongolia",
+    "New Zealand", "Norway", "Pakistan", "Peru", "Sierra Leone",
+    "Russian Federation", "Switzerland", "Sweden", "Turkey",
+    "United States of America", "United Kingdom of Great Britain and Northern Ireland",
+    "Zimbabwe"]
+
+year_over_2000 = un_corpus["year"] >= 2000
+countries_subset = un_corpus["country"].isin(countries_list)
+un_corpus_2000 = un_corpus[year_over_2000 & countries_subset]
 
 # Select a sample of 75 documents (50 for training, 25 for test)
 
@@ -34,6 +51,7 @@ def prepare_for_tagging(df, directory):
         split_by_lines = re.sub(r"([.?!])", r"\n\1\n", split_by_lines) # extra newline following EOS punctuation
         split_by_lines = re.sub(r"('s)", r"\n\1", split_by_lines) # possessive 's on separate line
         split_by_lines = re.sub(r"(n't)", r"\n\1", split_by_lines) # n't contraction on separate line
+        split_by_lines = re.sub(r"Mr\n.\n\n", r"Mr.\n", split_by_lines) # make Mr. one word
 
         filename = "{}_{}_{}.txt".format(row.ID, row.year, row.country)
 
---
+++ tag-ner-data.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+# Leslie Huang
+# Tag a few things automatically
+
+
+# O - none
+# I-LOC - location
+# I-PER - person
+# I-ORG - organization
+# I-MISC - misc named entity
+
+import argparse
+import collections
+import geotext
+import itertools
+from nltk.corpus import stopwords
+import os
+import re
+import sys
+
+directory = "training"
+files = os.listdir(directory)
+
+countries_list = ["Afghanistan", "Algeria", "Argentina", "Austalia",
+    "Bahrain", "Bhutan", "Brazil", "Bulgaria",
+    "Canada", "Chile", "Chad", "China", "Colombia",
+    "Denmark", "Egypt", "El Salvador", "Ethiopia", "France",
+    "Germany", "Ghana", "Iceland", "India", "Iraq", "Japan",
+    "Kuwait", "Libya", "Mali", "Mexico", "Myanmar", "Morocco", "Mongolia",
+    "New Zealand", "Norway", "Pakistan", "Peru", "Sierra Leone",
+    "Russian Federation", "Switzerland", "Sweden", "Turkey",
+    "United States of America", "United Kingdom of Great Britain and Northern Ireland",
+    "Zimbabwe"]
+
+orgs_list = ["United", "Nations", "European", "Union", "General", "Assembly", "Security", "Council"]
+
+progs_list = ["Programme", "Millenium", "Development", "Goals", "Declaration"]
+
+def extract_file(filename):
+    with open(os.path.join(directory, filename), "r") as f:
+        text = f.readlines()
+
+    return text
+
+def convert_tuples(text):
+    text_tag = []
+
+    for line in text:
+        if line != "\n":
+            text_tag.append((line.strip(), "O"))
+        else:
+            text_tag.append(("\n", None))
+
+    return text_tag
+
+def change_tags(text_tag):
+    new_tags = []
+    for (word, tag) in text_tag:
+        tag = word_country_tag(word, tag)
+        tag = tag_stopwords(word, tag)
+        tag = tag_orgs(word, tag)
+        tag = tag_progs(word, tag)
+
+        new_tags.append((word, tag))
+
+    return new_tags
+
+def word_country_tag(word, tag):
+    if word in " ".join(countries_list).split(" "):
+        return "I-LOC"
+    else:
+        return tag
+
+def tag_stopwords(word, tag):
+    if word.lower() in stopwords.words("english"):
+        return "O"
+    else:
+        return tag
+
+
+def tag_orgs(word, tag):
+    if word in orgs_list:
+        return "I-ORG"
+    else:
+        return tag
+
+def tag_progs(word, tag):
+    if word in progs_list:
+        return "I-MISC"
+    else:
+        return tag
+
+def convert_text_tags(text_tag):
+    text = []
+
+    for (word, tag) in text_tag:
+        if tag is not None:
+            text.append("{}\t{}\n".format(word, tag))
+        else:
+            text.append("\n\n")
+
+    return text
+
+
+for file in files:
+    text = extract_file(file)
+
+    text_tag = convert_tuples(text)
+
+    text = convert_text_tags(change_tags(text_tag))
+
+    with open(file, "w") as f:
+        f.writelines(text)
